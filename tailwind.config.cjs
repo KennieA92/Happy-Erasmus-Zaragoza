3be4a26fbb63b728bda6ef86f0516669c9ec9719
KennieA92 <91551710+KennieA92@users.noreply.github.com>
2023-04-17 15:25:34 +0200

    FB test

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,26 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-15deg) translateY(5rem)' },
+          '50%': { transform: 'rotate(15deg) translateY(0)' },
+        },
+        opacity: {
+          '0%, 100%': { opacity: 0.75, transform: 'rotate(-15deg) translateY(0)' },
+          '50%': { opacity: 1, transform: 'rotate(15deg) translateY(5rem)' },
+        },
+        translate: {
+          '0%, 100%': { transform: 'translateY(50%)' },
+          '50%': { transform: 'translateY(0)' },
+        }
+      },
+      animation: {
+        'spin-slow': 'opacity 10s infinite, spin 10s linear infinite',
+        wiggle: 'opacity 3s infinite, wiggle 3s ease-in-out infinite',
+      }
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
